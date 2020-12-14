cfc4c3a548c3846499a760922bffa9efe620a584
FRANCESCO <OLIVOTTI>
2020-12-14 15:41:58 +0100

    vue.js esercizio completato + BONUS

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/* Istruzioni:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere un’immagine presa anch’essa da un data   */
+
+
+
+
+var root = new Vue({
+el: '#root',
+data: {
+message: 'Hello World',
+
+ image: 'https://images8.alphacoders.com/108/1086806.jpg'
+}
+
+})
